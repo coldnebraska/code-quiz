2ae1cba0c6dd598fc5e4c244929aafb7b499a349
DBrush <coldnebraska@gmail.com>
2023-10-07 18:09:58 -0500

    added object with questions and answers. added timer

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,21 @@
 let quiz = document.getElementById("quiz")
-let content = document.getElementById("content-header")
+let contentHeader = document.getElementById("content-header")
 let highscoreButton = document.getElementById("highscore-button")
 let highscoreTitle = document.getElementById("highscore-title")
 let startButton = document.getElementById("start-button")
 let userList = document.getElementById("user-list")
 let scoreList = document.getElementById("score-list")
 let header = document.getElementById("header")
+let timer = document.getElementById("timer")
 
+// object of the five questions and their respective choices
+let questions = {
+    question: ["Commonly used data type DO NOT include:", "The condition in an if / else statement is enclosed with _____.", "Arrays in JavaScript can be used to store _____.", "String values must be enclosed within _____ when being assigned to variables.", "A very useful tool used during development and debugging for prining content to the debugger is:"],
+    choice1: ["Strings", "Quotes", "Numbers and Strings", "Commas", "JavaScript"],
+    choice2: ["Boolean", "Curly Brackets", "Other Arrays", "Curly Brackets", "Terminal/Bash"],
+    choice3: ["Alerts", "Parenthesis", "Booleans", "Quotes", "For Loops"],
+    choice4: ["Numbers", "Square Brackets", "All of the Above", "Parenthesis", "Console.log"],
+}
 
 let initials =  ["DB"]
 let scores =  [0]
@@ -49,8 +58,23 @@ function viewHighscores() {
     }
 }
 
+function countdown() {
+    // TODO: change timer to 60
+    let timeLeft = 5
+
+    let timeInterval = setInterval(function() {
+        timeLeft--
+        timer.textContent = timeLeft
+    if (timeLeft == 0) {
+        clearInterval(timeInterval)
+        endScreen()
+    }
+    }, 1000)
+}
+
 function startQuiz() {
-    
+    quiz.innerHTML = "" // removes main content
+    countdown()
 }
 
 // Pulls local data for previous scores (if any)
